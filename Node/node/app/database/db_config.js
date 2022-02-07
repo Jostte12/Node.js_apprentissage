94fce9850bfc56129c2629853564d21a6d6587a2
Josette <jostte84@gmail.com>
2022-02-07 19:35:13 +0300

    Edit console.log

--- Node/node/app/database/db_config.js
+++ Node/node/app/database/db_config.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://localhost:27017/voitures', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("Connected successfully"))
+.then(() => console.log("Connected successfully !"))
 .catch((err) => console.log("Connection failed", err))
